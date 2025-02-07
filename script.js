79b9a22beb595f56b0ead8817b5752acab6afea1
Atikulla2001 <atikullahasib2001@gmail.com>
2025-02-07 18:11:08 +0600

    file is added

---
+++ script.js
@@ -0,0 +1,160 @@
+// call back function // 
+
+// function hasib (a){
+//     console.log("call back function ")
+//     a();
+// }
+
+// function atikulla () {
+//     console.log("Mern 2405")
+// }
+
+
+// hasib(atikulla);
+
+// ======== ======= ======= //
+
+
+
+// ======= reversee ======= //
+
+
+
+// let array = ["mango", "apple", "ball", "cat", "dog"]
+
+// let reversee = array.reverse();
+
+// console.log(reversee, "ulta");
+
+
+
+
+
+
+// ======= shortt ======= //
+
+
+// let array = ["mango", "apple", "ball", "cat", "dog"]
+
+// let shortt = array.sort();
+
+// console.log(shortt, "serial mantain kore");
+
+
+
+
+
+// ======= index of ======= //
+
+// let array = ["mango", "apple", "ball", "cat", "dog"]
+
+// let indexoff = array.indexOf("dog");
+
+// console.log(indexoff);
+
+
+
+
+
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+
+
+// ======= reducee ======= //
+
+// let reducee = array.reduce((back , next )=>{
+
+//     return back + next ;
+
+//     // console.log(next)
+
+
+// }, 4)
+
+//  console.log(reducee, "mathematical operations kore")
+
+
+
+// ======= find ======= //
+
+// let findd = array.find((x)=>{
+//     return x > 3;
+// })
+
+// console.log(findd);
+
+
+
+
+// ======= filter ======= //
+
+// let filterr = array.filter((x)=>{
+//     // console.log(x);
+
+//     return x <= 4 ;
+
+// })
+
+// console.log(filterr);
+
+
+
+
+// let bd = array.forEach((x,y,z)=>{
+
+//     // return 10 ; 
+
+//     console.log(x, y, z);
+
+//     console.log(x + "atikulla Hasib");
+
+//     console.log(x + 2, "item, index, array, +, -");
+
+
+// })
+
+// console.log(bd);
+
+
+
+// ========== map return ======== //
+
+// let bangladesh = array.map((a,b,c)=>{
+
+//     // return 20 ;
+
+//     console.log(array[4]);
+
+//     console.log(a, b, c);
+
+//     console.log(a + "hasib");
+
+// })
+
+// // console.log(bangladesh , " map return korte pabe");
+
+
+
+
+
+
+
+
+
+// ======== for loop ======= //
+
+// console.log(array[0]); 
+
+// for (let a = 0; a < array.length; a++) {
+
+
+//     console.log(a);  === index number
+
+//     console.log(array[0]); 
+
+//     console.log(array);
+    
+    
+// }
+
